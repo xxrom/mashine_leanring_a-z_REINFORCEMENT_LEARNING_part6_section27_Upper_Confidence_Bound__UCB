166fdeac88ddd9c2960c3fc365a290d77f4b776b
Nikita Chernyshov <xpom55@mail.ru>
2018-03-25 14:03:30 +0300

    finish algorithm, really interesting results, almost twice the end result ( 2200 instead of 1200 when we used random method)

--- upper_confidence_bound.py
+++ upper_confidence_bound.py
@@ -22,24 +22,37 @@ N = 10000 # количество пользователей
 d = 10 # количество версий рекламы
 ads_selected = [] # выбор рекламы на каждом шаге
 numbers_of_selections = [0] * d # читерский метод создания вектора [0,0,0...]
-sums_of_rewards = [0] * d
+sums_of_rewards = [0] * d # вознагарждение по рекламам
+total_reward = 0 # вознаграждение общее (сумма по всем рекламам)
 
 # step 2
+# первые d шагов мы проходимся по всем рекламам, чтобы набрать
+# какую-то статистику для корректной работы алгоритма
 for n in range(0, N) : # [0,10000)
   max_upper_bound = 0 # максимальная граница рекламы максимальной
   ad = 0 # индекс максимальной рекламы
   for i in range(0, d) :
     if (numbers_of_selections[i] > 0) :
       average_reward = sums_of_rewards[i] / numbers_of_selections[i]
-      delta_i = math.sqet(3/2 * math.log(n + 1) / number_of_selections[i])
+      delta_i = math.sqrt(3/2 * math.log(n + 1) / numbers_of_selections[i])
       upper_bound = average_reward + delta_i
     else :
-      upper_bound = 1e400 # max item
+      upper_bound = 1e400 # max item сюда попадаем только первые d шагов
     if (upper_bound > max_upper_bound) :
       max_upper_bound = upper_bound
       ad = i
 
+  # выбранная реклама закидываем каждый шаг
+  ads_selected.append(ad)
+  # количество выборов каждого типа рекламы
+  numbers_of_selections[ad] = numbers_of_selections[ad] + 1
+  # получили вознаграждение из базы данных (реакция пользователя)
+  reward = dataset.values[n, ad]
+  # применили вознаграждение
+  sums_of_rewards[ad] = sums_of_rewards[ad] + reward
+  total_reward = total_reward + reward
 
+# самая продаваемая реклама будет с 4 index , получается 5 столбец в базе
 
 
 
